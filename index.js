acbefdb6657fad5cf2f54f4e4acd62dc719fdfa1
scorjeff <jf.boisgontier@gmail.com>
2025-08-02 14:38:43 +0200

    chgt structure

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/api/status', (req, res) => {
   res.json({ message: '✅ API Render opérationnelle' });
 });
 
+app.get('/', (req, res) => {
+  res.send('🎯 API TruthTalent est en ligne ! Utilisez les routes /api/*');
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Serveur en ligne sur le port ${PORT}`);
 });
