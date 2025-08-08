425478730705f8e81525c83d63f50573d3df99f6
scorjeff <jf.boisgontier@gmail.com>
2025-08-08 16:53:24 +0200

    test deploy node

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,5 +1,6 @@
 // utils/supabase.ts
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '../@supabase/supabase-js'
+import { createClient } from '../node_modules/@supabase/supabase-js'
 
 const supabaseUrl = 'https://venldvzkjzybpffrtkql.supabase.co'
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '' // clé sécurisée dans .env.local
