b4ddbe950ee988c44088456db988a3d32ae3e338
scorjeff <jf.boisgontier@gmail.com>
2025-08-08 15:30:19 +0200

    maj client supabase node_module

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,5 +1,6 @@
 // utils/supabase.ts
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js/src';
+//import { createClient } from '../node_modules/@supabase/supabase-js/src'
 
 const supabaseUrl = 'https://venldvzkjzybpffrtkql.supabase.co';
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || ''; // dans .env.local
