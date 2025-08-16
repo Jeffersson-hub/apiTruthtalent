c859e513e7c04ef80e3a781ad6bf805099c1baa5
scorjeff <jf.boisgontier@gmail.com>
2025-08-16 10:19:08 +0200

    retour requete post

--- pages/api/parse.ts
+++ pages/api/parse.ts
@@ -37,7 +37,7 @@ export default async function handler(
   }
 
   
-  if (req.method !== 'GET') {
+  if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Méthode non autorisée' });
   }
 
