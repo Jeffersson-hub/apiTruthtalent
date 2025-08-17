e9178a03b892e4715237a02c44bb106e1073ce9b
scorjeff <jf.boisgontier@gmail.com>
2025-08-17 12:35:34 +0200

    modif middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(_request: NextRequest) {
+export function middleware() {  // eslint-disable-line @typescript-eslint/no-unused-vars
   const response = NextResponse.next();
   response.headers.set('Access-Control-Allow-Origin', '*');
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
